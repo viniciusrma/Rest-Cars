efe1151c3f10892405e17211a792fe65330aaa5f
viniciusrma <viniciusrma.dev@gmail.com>
2020-12-24 17:18:53 -0300

    implemented basic crud for rendering info from the db and added cors to backend

--- web-cars/src/App.tsx
+++ web-cars/src/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Routes from './routes';
+import Header from './components/header';
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes />
     </BrowserRouter>
   );
-};
+}
 
 export default App;
--- web-cars/src/pages/cars/index.tsx
+++ web-cars/src/pages/cars/index.tsx
@@ -1,7 +1,61 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Table, Button } from 'react-bootstrap';
+import api from '../../services/api';
+
+interface ICar {
+  id: number;
+  brand: string;
+  model: string;
+  description: string;
+  year: number;
+}
 
 const Cars: React.FC = () => {
-  return <h1>Cars Page</h1>;
+  const [cars, setCars] = useState<ICar[]>([]);
+
+  useEffect(() => {
+    loadCars();
+  }, []);
+
+  async function loadCars() {
+    const response = await api.get('/cars');
+    console.log(response);
+    setCars(response.data);
+  }
+
+  return (
+    <div className="container">
+      <br />
+      <h1>Cars Page</h1>
+      <br />
+      <Table striped bordered hover className="text-center">
+        <thead>
+          <tr>
+            <th>Brand</th>
+            <th>Model</th>
+            <th>Description</th>
+            <th>Year</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {cars.map((car) => (
+            <tr key={car.id}>
+              <td>{car.brand}</td>
+              <td>{car.model}</td>
+              <td>{car.description}</td>
+              <td>{car.year}</td>
+              <td>
+                <Button size="sm">Edit</Button>{' '}
+                <Button size="sm"variant="info">View</Button>{' '}
+                <Button size="sm" variant="danger">Delete</Button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </div>
+  );
 };
 
 export default Cars;
